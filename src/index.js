7e92285be389366042e8edf745c0e5ebf7c5ca80
Eliza Hynek <eliza.hynek@gmail.com>
2023-04-05 11:46:50 +0200

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 //import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -16,4 +15,4 @@ root.render(
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-);
+document.getElementById('root'));
